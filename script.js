1b0e2d29dac61f69083e6d3169dbf816cca244e4
daviddollanarte <87336994+daviddollanarte@users.noreply.github.com>
2021-11-08 13:52:19 -0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.getElementById("showCollapse").addEventListener("mouseover", function () {
     document.getElementById("trigger").click();
 });
+if (document.getElementById("showCollapse2")) {
+    document.getElementById("showCollapse2").addEventListener("mouseover", function () {
+        document.getElementById("trigger2").click();
+    });
+};
+if (document.getElementById("showCollapse3")) {
+    document.getElementById("showCollapse3").addEventListener("mouseover", function () {
+        document.getElementById("trigger3").click();
+    });
+};
+if (document.getElementById("change1")){
+    let textChange = document.getElementById("change1");
+    textChange.addEventListener("mouseover", function () {
+        textChange.innerHTML = `solo`;
+        textChange.classList.add("badge");
+        textChange.classList.add("bg-danger");
+    });
+    textChange.addEventListener("mouseout", function () {
+        textChange.innerHTML = `mejor`;
+        textChange.classList.remove("badge");
+        textChange.classList.remove("bg-danger");
+    })
+};
